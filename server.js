0c1d0d6a83560d7ca0a5011236d8ef55fee056ef
Beatriz Alcantara <beatrizalcantara813@gmail.com>
2024-02-13 19:54:03 -0300

    enable all cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from "express"
 import { CategoryEndpoint, BookEndpoint  } from "./Endpoints/Index.js"
 const app = express()
+var cors = require('cors')
 const port = 3000
 
 app.use(express.json())
+app.use(cors())
 
 CategoryEndpoint.RegisterCategoryEndpoints(app)
 BookEndpoint.RegisterBookEndpoint(app)
